ec37d4927ed14166d8907ec5a7d1e4b28aabb47e
hergimerc <hzsn@hzsn.my.id>
2023-06-24 21:43:46 +0700

    164. More ways of creating arrays

--- script.js
+++ script.js
@@ -387,3 +387,28 @@ movements.sort((a, b) => b - a);
 console.log(movements);
 
 */
+
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+
+x.fill(1, 3, 5);
+x.fill(1);
+
+// Array.from
+const y = Array.from({ length: 7 }, (cur, i) => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
